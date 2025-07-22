ced863a826d1aee3de42ef0dfcc4cc83101172cd
Tasneem <tasneemm967@gmail.com>
2025-07-22 22:35:04 +0200

    Initial commit

---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" crossorigin="anonymous">
+    <title>Todos</title>
+</head>
+<body>
+    
+    <div class="container">
+        <h1 class="text-center my-5">TODOS PAGE</h1>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">
+                        Todos
+                    </div>
+                
+                    <div class="card-body">
+                        <ul class="list-group">
+                            @foreach($todos as $todo)
+                                <li class="list-group-item">
+                                    
+                                    {{ $todo->name }}
+                                    <button class="btn btn-primary btn-sm float-right">View</button>
+                    
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
