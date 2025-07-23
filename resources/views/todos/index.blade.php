3436197e9eceb0272c5bd6ee104ce3e97f414c14
Tasneem <tasneemm967@gmail.com>
2025-07-23 23:16:30 +0200

    Add layout styles

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,38 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" crossorigin="anonymous">
-    <title>Todos</title>
-</head>
-<body>
+@extends('layouts.app')
+
+
+@section('content')
     
-    <div class="container">
-        <h1 class="text-center my-5">TODOS PAGE</h1>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-header">
-                        Todos
-                    </div>
+    <h1 class="text-center my-5">TODOS PAGE</h1>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">
+                    Todos
+                </div>
+            
+                <div class="card-body">
+                    <ul class="list-group">
+                        @foreach($todos as $todo)
+                            <li class="list-group-item">
+                                
+                                {{ $todo->name }}
+                                <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right">View</a>
                 
-                    <div class="card-body">
-                        <ul class="list-group">
-                            @foreach($todos as $todo)
-                                <li class="list-group-item">
-                                    
-                                    {{ $todo->name }}
-                                    <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right">View</a>
-                    
-                                </li>
-                            @endforeach
-                        </ul>
-                    </div>
+                            </li>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
         </div>
     </div>
-    
-</body>
-</html>
+
+
+@endsection
