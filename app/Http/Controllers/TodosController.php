330fca9d4d25799afbd441a4c531c9102fdff6e0
Tasneem <tasneemm967@gmail.com>
2025-07-26 21:44:42 +0200

    Implement flash & complete features

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -38,11 +38,68 @@ class TodosController extends Controller
 
         $todo->save();
 
+        session()->flash('success', 'Todo created successfully.');
+
+
         return redirect('/todos');
 
     }
 
 
+<<<<<<< Updated upstream
+=======
+    public function edit($todoId) {
+        
+        $todo = Todo::find($todoId);
+
+        return view('todos.edit')->with('todo', $todo);
+    }
+
+    public function update($todoId) {
+
+        $this->validate(request(), [
+            'name' => 'required',
+            'description' => 'required'
+        ]);
+
+        $data = request()->all();
+
+        $todo = Todo::find($todoId);
+
+        $todo->name = $data['name'];
+        $todo->description = $data['description'];
+
+        $todo->save();
+
+        session()->flash('success', 'Todo updated successfully.');
+
+        return redirect('/todos');
+    }
+
+    
+    public function destroy($todoId) {
+        $todo = Todo::find($todoId);
+
+        $todo->delete();
+
+        session()->flash('success', 'Todo deleted successfully.');
+
+        return redirect('/todos');
+    }
+
+    public function complete(Todo $todo) {
+
+        $todo->completed = true;
+
+        $todo->save();
+
+        session()->flash('success', 'Todo completed successfully.');
+
+        return redirect('/todos');
+
+    }
+    
+>>>>>>> Stashed changes
 
 
 
